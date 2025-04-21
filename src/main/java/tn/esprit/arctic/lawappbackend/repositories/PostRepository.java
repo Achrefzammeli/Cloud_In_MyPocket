64ba41d02b77b97c1b69d5fb40e1b30cad91d8b0
Mahe-Kefi12 <Maha.Kefi@esprit.tn>
2025-04-21 22:06:16 +0100

    commit message mahe

--- src/main/java/tn/esprit/arctic/lawappbackend/repositories/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package tn.esprit.arctic.lawappbackend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import tn.esprit.arctic.lawappbackend.entities.Post;
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long> {
-}
