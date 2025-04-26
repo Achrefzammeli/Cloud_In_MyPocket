d472fef83a958f4b123ba029d9e6d38f24fd212d
maissahammami <148292631+maissahammami@users.noreply.github.com>
2025-04-26 23:03:29 +0100

    "remove cross"

--- src/main/java/tn/esprit/cloud_in_mypocket/Controller/ApprenantController.java
+++ src/main/java/tn/esprit/cloud_in_mypocket/Controller/ApprenantController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 @RestController
 //@RequestMapping("/apprenants")
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 public class ApprenantController {
 
     @Autowired
